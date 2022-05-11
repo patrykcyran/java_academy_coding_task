599f0982b9808690626f414f1f9968f493ff6264
patrykcyran <patrykcyranek@gmail.com>
2022-05-11 08:23:50 +0200

    Final version

--- src/main/java/patryk/cyran/Main.java
+++ src/main/java/patryk/cyran/Main.java
@@ -1,7 +1,28 @@
 package patryk.cyran;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        SortedMap<Character, Integer> letters = new TreeMap<>();
+
+        String argument = String.join("", args).toLowerCase(Locale.ROOT);
+
+        for (int i = 0; i < argument.length(); i++) {
+            Character currChar = argument.charAt(i);
+            if (!letters.containsKey(currChar)) {
+                letters.put(currChar, 1);
+            } else {
+                int currSize = letters.get(currChar);
+                letters.replace(currChar, ++currSize);
+            }
+        }
+
+        System.out.println("Counted letters:");
+
+        for (var entrySet :letters.entrySet()) {
+            System.out.println(entrySet.getKey() + " = " + entrySet.getValue());
+        }
     }
 }
